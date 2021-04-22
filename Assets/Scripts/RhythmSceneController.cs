9e35405c28e78d07af6e29cbb480eafcd2c4ec6a
Rungsikorn Rungsikavanich <rungsikorn@Rungsikorns-MacBook-Pro.local>
2021-04-22 12:58:25 +0700

    init

---
+++ Assets/Scripts/RhythmSceneController.cs
@@ -0,0 +1,36 @@
+﻿using UnityEngine;
+using System.IO;
+
+public class RhythmSceneController : MonoBehaviour
+{
+    [SerializeField]
+    private string drumTrackFileName;
+    private byte[] _midiTrackFileBuffer;
+
+    void Start()
+    {
+        // Loading drum track from midi file
+        _midiTrackFileBuffer = File.ReadAllBytes($"{Application.dataPath}/Resources/{drumTrackFileName}.mid");
+        // Below this please implement scene initialize
+        // from MIDI track above to any kind of rhythm game play scene
+        // *  please ensure the best approach to draw the scene for memory efficiency 
+        // ** any kind of scene is welcome ( both 2D or 3D )
+        //
+        // Hint for MIDI track 
+        // this MIDI track represent 4 bars of drum track in Cha Cha Cha rhythm style
+        // Looping for pad note
+        // C#2 (37) is rim snare sound
+        // C#3 (49) is Cymbal
+        // D2  (38) snare
+        // C2  (36) is Bass drum
+        // C3  (48) is High tom
+        // G2  (43) is Floor tom
+        // G2  (43) is Floor tom
+        Debug.Log(_midiTrackFileBuffer);
+    }
+
+    void Update()
+    {
+        
+    }
+}
